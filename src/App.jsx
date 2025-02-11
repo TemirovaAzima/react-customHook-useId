a0893cf7e6ebe8c88f2dc5994370d548cf94c533
TemirovaAzima <temirovaazima9@gmail.com>
2025-02-11 21:53:45 +0500

    Custom hook with useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import {useFetch} from "./useFetch.js";
+import {useCounter} from "./useCounter.js";
+import {useState} from "react";
 
 const App = () => {
-    const {data, loading, error} = useFetch('https://jsonplaceholder.typicode.com/users');
-
-    if(loading) return <p>Loading...</p>;
-    if(error) return <p>Error : {error.message}</p>;
+    const {count, increment, decrement, incrementByAmount, decrementByAmount,reset} = useCounter();
+    const [inputValue, setInputValue] = useState(0);
     return (
-        <ul>
-            {data.map((user)=>(
-                <li key={user.id}>{user.name}</li>
-            ))}
-        </ul>
-    );
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+            <input
+                type="number"
+                value={inputValue}
+                placeholder='enter number'
+                onChange={(e) => setInputValue(e.target.value)}/>
+            <button onClick={() => {
+                incrementByAmount(Number(inputValue));
+                 setInputValue(0)}}
+                >Add</button>
+            <button onClick={() => {
+                decrementByAmount(Number(inputValue))
+                setInputValue(0)}}
+               >Subtract</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    )
 }
 export default App
 
